36e3ccb3c0b69702a2e7614157a1e50c3855727f
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-11-05 17:47:40 -0800

    Completed GET requests to /api/posts/, /api/posts/:id/ and /api/posts/:id/comments.

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -3,10 +3,6 @@ const router = express.Router();
 
 const db = require('../data/db');
 
-router.get('/', (req, res) => {
-    res.status(200).send('hello from the GET /posts endpoint');
-  });
-
 router.post('/', (req, res) => {
     const post = req.body;
 
@@ -36,11 +32,11 @@ router.post('/', (req, res) => {
                 success: false,
                 error: "There was an error while saving the post to the database"
             }
-        ))
-    }
-  })
+        )
+    )}
+})
 
-  router.post('/:id/comments', async (req, res) => {
+router.post('/:id/comments', async (req, res) => {
     const id = req.params.id;
     const comment = {...req.body, post_id: id}
 
@@ -83,4 +79,82 @@ router.post('/', (req, res) => {
     }
   })
 
-  module.exports = router;
+router.get('/', (req, res) => {
+    db.find()
+    .then(posts => res.status(200).json(
+        {
+            success: true,
+            posts: posts
+        }
+    ))
+    .catch(() => res.status(500).json(
+        {
+            success: false,
+            error: "The posts information could not be retrieved."
+        }
+    ))
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+
+    try {
+        const post = await db.findById(id);
+        if (post.length === 0) {
+            res.status(404).json(
+                {
+                    success: false,
+                    message: "The post with the specified ID does not exist."
+                }
+            )
+        } else {
+            res.status(200).json(
+                {
+                    success: true,
+                    post: post
+                }
+            )
+        }
+    }
+    catch (err) {
+        res.status(500).json(
+            {   
+                success: false,
+                error: "The post information could not be retrieved."
+            }
+        )
+    }
+})
+
+router.get('/:id/comments', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const postComments = await db.findPostComments(id);
+        if (postComments.length > 0) {
+            res.status(200).json(
+                {
+                    success: true,
+                    comments: postComments
+                }
+            )
+        } else {
+            res.status(404).json(
+                {
+                    success: false,
+                    message: "The post with the specified ID does not exist."
+                }
+            )
+        } 
+    }
+    catch (err) { 
+        res.status(500).json(
+            {
+                success: false,
+                error: "The comments information could not be retrieved."
+            }
+        )
+    }
+})
+
+module.exports = router;
